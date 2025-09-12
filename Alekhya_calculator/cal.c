b9d57bfaa415d5120d9aa2920753c04219f4fa86
Alekhya-0901 <alekhya.m@benisontech.com>
2025-09-12 07:20:58 +0000

    added cal.c into Alekhya_calculator folder

---
+++ Alekhya_calculator/cal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, res;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+    switch (operator) {
+        case '+':
+            res = num1 + num2;
+            printf("Res: %.2lf\n", res);
+            break;
+        case '-':
+            res = num1 - num2;
+            printf("Res: %.2lf\n", res);
+            break;
+        case '*':
+            res = num1 * num2;
+            printf("Res: %.2lf\n", res);           
+            break;
+        case '/':
+            if (num2 != 0) {
+                res = num1 / num2;
+                printf("Res: %.2lf\n", res);
+            } else {
+                printf("Error\n");
+            }
+            break;
+        default:
+            printf("Invalid\n");
+    }
+
+    return 0;
+}
+
